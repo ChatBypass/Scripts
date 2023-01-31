29c936f1152915422aceebd8197fedef07efc1a4
ChatBypass <113918659+ChatBypass@users.noreply.github.com>
2023-01-31 18:37:58 +0100

    Skincodes v2 (20's)
    wow wow wow wow hey hey hey hey
    oh waw

---
+++ Arsenal-skincodes.lua
@@ -0,0 +1,119 @@
+local args = {
+    [1] = "ROLVE"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "POKE"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "GARCELLO"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "ANNA"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "CBROX"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "F00LISH"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "Bandites"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "EPRIKA"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "John"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "Flamingo"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "xonae"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "KITTEN"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "PET"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "E"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "TROLLFACE"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "POG"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "BLOXY"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "10keni"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "BRUTE"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(0.3)
+local args = {
+    [1] = "The Banana Man"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
